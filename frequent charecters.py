d675a8502a79696d75785a76ff92cd03c15e691c
Abitha <86874240+codecreation01@users.noreply.github.com>
2021-07-28 12:02:39 +0530

    created frequent charecters

---
+++ frequent charecters.py
@@ -0,0 +1,8 @@
+test_str="mississippi"
+all_freq={}
+for i in test_str:
+    if i in all_freq:
+        all_freq[i]+=1
+    else:
+        all_freq[i]=1
+print("count of all charecters in mississippi is:\n"+str(all_freq))
